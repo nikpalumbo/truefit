a59282a0e712e8dcb0cf29c3c4ca564ab70979a5
Nicola <873910+palumbonicola@users.noreply.github.com>
2025-05-05 21:28:03 +0200

    Adds variables to conf

--- configuration.py
+++ configuration.py
@@ -2,7 +2,29 @@
 User-specific configuration schema for LangGraph sessions.
 """
 
-from pydantic import BaseModel
+import os
+from dataclasses import dataclass, fields
+from typing import Any, Optional
 
-class Configuration(BaseModel):
-    user_id: str
+from langchain_core.runnables import RunnableConfig
+from dataclasses import dataclass
+
+@dataclass(kw_only=True)
+class Configuration:
+    """The configurable fields for the chatbot."""
+    user_id: str = "default-user"
+
+    @classmethod
+    def from_runnable_config(
+        cls, config: Optional[RunnableConfig] = None
+    ) -> "Configuration":
+        """Create a Configuration instance from a RunnableConfig."""
+        configurable = (
+            config["configurable"] if config and "configurable" in config else {}
+        )
+        values: dict[str, Any] = {
+            f.name: os.environ.get(f.name.upper(), configurable.get(f.name))
+            for f in fields(cls)
+            if f.init
+        }
+        return cls(**{k: v for k, v in values.items() if v})
